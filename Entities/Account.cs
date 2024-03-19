74badaaf19533c0b991c5d539106fd9e69b9ec7d
DjDecay21 <mrychert12@gmail.com>
2024-03-19 08:58:35 +0100

    Initial commit

---
+++ Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿namespace TheBankProject.Entities
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string NumberAccount { get; set; }
+        public decimal Value { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual List<Transaction> Transactions { get; set; }
+
+
+    }
+}
---
+++ Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBankProject.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FromAccountId { get; set; }
+        public virtual Account FromAccount { get; set; }
+
+        public int ToAccountId { get; set; }
+        public virtual Account ToAccount { get; set; }
+        public decimal Value { get; set; }
+        public string Title { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool Status { get; set; }
+    }
+}
